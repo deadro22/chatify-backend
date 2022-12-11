ea1e6eca29e3edf8d5334dbced4ea9da0109b7c8
unknown <gamemangamer8@gmail.com>
2022-12-11 13:49:31 +0100

    'fr_chs

--- db/mongoConnection.js
+++ db/mongoConnection.js
@@ -3,16 +3,7 @@ const mongoose = require("mongoose");
 module.exports = function () {
   mongoose.set("strictQuery", false);
   console.log(process.env.DB_CONNECTION_URI);
-  mongoose.connect(
-    process.env.DB_CONNECTION_URI,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    },
-    () => {
-      console.log("Connected to db");
-    }
-  );
+  mongoose.connect(process.env.DB_CONNECTION_URI, {}, () => {
+    console.log("Connected to db");
+  });
 };
